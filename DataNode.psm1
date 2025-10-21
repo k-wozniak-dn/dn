271f62c2ef34f2d2d4d1be8047899a9d916cd30d
Krzysztof Wozniak <krzysztof.roman.wozniak@gmail.com>
2025-10-22 00:25:18 +0200

    import-n-get

--- DataNode.psm1
+++ DataNode.psm1
@@ -1,101 +1,92 @@
 enum FileFormatEnum {
-    PSDataFile;
-    Json;
-    Xml;
-    Csv;
+    psd1;
+    json;
+    xml;
+    csv;
+}
+
+enum ItemType {
+    Section;
+    Item;
 }
 
 
-
-function Import-DN {
+function Import-Dn {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
-        [string] $Path,
-        [FileFormatEnum] $Format = [FileFormatEnum]::PSDataFile
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [System.IO.FileInfo] $FileInfo
     )
     Begin {
-        if (-not (Test-Path -Path $Path)) {
-            throw "Path '$Path' doesn't exist."
-        }
     }
 
     Process {
-        switch ($Format) {
-            { $_ -eq [FileFormatEnum]::PSDataFile } { $dn = Import-PowerShellDataFile -Path $Path ; break; }
-            default { throw "File format '$Format' not supported." }
+        try {
+            $ext = $FileInfo.Extension;
+            switch ($ext) {
+                { $_ -eq ("." + [FileFormatEnum]::psd1) } { 
+                    $dn = Import-PowerShellDataFile -Path $FileInfo.FullName ;
+                    break; 
+                }
+                default { throw "File format '$ext' not supported." }
+            }
+            $dnWrapper = [PSCustomObject] @{ "Path" = $FileInfo.FullName; "HT" = $dn; }
+            Write-Output $dnWrapper             
+        }
+        catch {
+            Write-Error $_
         }
     }
 
     End {
-        $dnWrapper = [PSCustomObject] @{ "Path" = $Path; "HT" = $dn; }
-        Write-Output $dnWrapper
     }
 }
 
-Set-Alias -Name ipdn -Value Import-DN
+Set-Alias -Name ipdn -Value Import-Dn
 
-function Get-Section {
+function Get-DnItem {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)] [PSCustomObject] $dn,
-        [Parameter(Mandatory = $true)] [string] $SectionKey,
-        [switch] $CreateDefault
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)] [PSCustomObject] $Dn,
+        [Parameter(Mandatory = $true)] [string] $Path
     )
 
+    Begin {
+        $Parent = Split-Path $Path -Parent;
+        $Leaf =  Split-Path $Path -Leaf;
+        if ([string]::Empty -eq $Parent) { 
+            $Parent = $Leaf ; 
+            $Leaf = [string]::Empty 
+        }
+        if ([string]::Empty -eq $Parent) { throw "Incorrect path '$Path'." }
+    }
+
     Process {
-        [hashtable] $dnht = $dn.HT;
-        if ($dnht.ContainsKey($SectionKey)) { $section = $dnht.$SectionKey ; }
-        elseif ($CreateDefault) { $section = $dnht = @{} ;}
-        else { throw "Section '$SectionKey' not found." }
+        [hashtable] $DnHT = $dn.HT;
+        $MatchingSectionKeys = $DnHT.Keys | Where-Object { $_ -like $Parent }
+
+        foreach ($SectionKey in $MatchingSectionKeys) {
+                $Section = $DnHT[$SectionKey];
+                if ([string]::Empty -eq $Leaf) {
+                    $ItemWrapper = [PSCustomObject] @{"ItemType" = [ItemType]::Section; "Key" = $SectionKey; "HT" = $Section }
+                    Write-Output $ItemWrapper                    
+                }
+                else {
+                    $MatchingItemKeys = $Section.Keys | Where-Object { $_ -like $Leaf }
+                    foreach ($ItemKey in $MatchingItemKeys) {
+                        $Item = $Section[$ItemKey];
+                        $ItemWrapper = [PSCustomObject] @{"ItemType" = [ItemType]::Item; "Key" = $ItemKey; "HT" = $Item }
+                        Write-Output $ItemWrapper  
+                    }
+                }
+            }
     }
 
     End {
-        $sectionWrapper = [PSCustomObject]@{ "SectionKey" = $SectionKey; "HT" = $section }
-        Write-Output $sectionWrapper
     }
 }
 
-Set-Alias -Name gsec -Value Get-Section
-
-function Get-SectionItem {
-    [CmdletBinding()]
-    Param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)] [PSCustomObject] $Section,
-        [string[]] $Keys,
-        [switch] $CreateDefault
-    )
-
-    Process {
-        $sectionHT = $Section.HT
-
-        if (($null -eq $Keys) -or (($Keys -is [array]) -and ($Keys.Count -eq 0))) {
-            foreach ($key in $sectionHT.Keys) {
-                $item = $sectionHT[$key]
-                $itemWrapper = [PSCustomObject] @{ "ItemKey" = $key; "HT" = $item }
-                Write-Output $itemWrapper
-            }
-        }
-        else {
-            foreach ($key in $Keys) {
-                if ($sectionHT.ContainsKey($key)) {
-                    $item = $sectionHT[$key]                    
-                }
-                elseif ($CreateDefault) {
-                    $item = $sectionHT[$key] = @{}
-                }
-                else {
-                    throw "Key '$key' is missing in the section $($Section.SectionKey)."
-                }
-
-                $itemWrapper = [PSCustomObject] @{ "ItemKey" = $key; "HT" = $item }
-                Write-Output $itemWrapper
-            }
-        }
-    }
-}
-
-Set-Alias -Name gseci -Value Get-SectionItem
+Set-Alias -Name gdni -Value Get-DnItem
 
 Export-ModuleMember -Function *
 Export-ModuleMember -Alias *
