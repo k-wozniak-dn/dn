aec2ee9acfb5f090bc63dfda5cb213896f76bf34
Krzysztof Wozniak <krzysztof.roman.wozniak@gmail.com>
2025-11-02 00:12:20 +0100

    var-ranames

--- DataNode.psm1
+++ DataNode.psm1
@@ -1,15 +1,32 @@
+#region const
+enum ItemType {
+    dn;
+    s;
+    i;
+    p;
+}
+
+enum dni {
+    it;
+    k;
+    p;
+    v;
+    vt;
+}
+
+enum dnPath {
+    PathType;
+    SectionPart;
+    ItemPart;
+    PropertyPart;
+    ParentPath;
+}
+
 enum FileFormatEnum {
     psd1;
     json;
     xml;
     csv;
-}
-
-enum ItemType {
-    DataNode;
-    Section;
-    Item;
-    Property;
 }
 
 enum ValueType {
@@ -20,7 +37,11 @@ enum ValueType {
     Boolean;
 }
 
+Set-Variable -Name 'PathDelimiter' -Value '/' -Option ReadOnly
 
+#endregion
+
+#region l-0
 function Copy-HashtableDeep {
     [CmdletBinding()]
     param (
@@ -38,21 +59,83 @@ function Get-DnPath {
         [Parameter(Mandatory = $true)] [string] $Path
     )
     Begin {
-        $parts = $Path -split '[\\/]+';
+        $parts = $Path -split [regex]::Escape($PathDelimiter);
         $count = ($parts).Count;
         if ($count -gt 3) { throw "Incorrect path '$path'." }
         $parts | ForEach-Object { if ([string]::Empty -eq $_) {throw "Incorrect path '$path'."}}
+    }
 
+    End {
         return [PSCustomObject] @{ 
-            "PathType" = ($count -eq 1) ? [ItemType]::Section : (($count -eq 2) ? [ItemType]::Item : [ItemType]::Property); 
-            "Section" = $parts[0]; 
-            "Item" = $count -gt 1 ? $parts[1] : $null ; 
-            "Property" = $count -gt 2 ? $parts[2] : $null ;
-            "ParentPath" = ($count -eq 1) ? $null : (($count -eq 2) ? $parts[0] : $parts[0] + "/" + $parts[1]); 
-            }
+            [dnPath]::PathType = ($count -eq 1) ? [ItemType]::s : (($count -eq 2) ? [ItemType]::i : [ItemType]::p); 
+            [dnPath]::SectionPart = $parts[0]; 
+            [dnPath]::ItemPart = $count -gt 1 ? $parts[1] : $null ; 
+            [dnPath]::PropertyPart = $count -gt 2 ? $parts[2] : $null ;
+            [dnPath]::ParentPath = ($count -eq 1) ? $null : (($count -eq 2) ? $parts[0] : $parts[0] + $PathDelimiter + $parts[1]); 
+            }        
     }
 }
 
+function New-DnItem {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)] [string] 
+        [Alias("it")]
+        [ValidateScript({ 
+            ($_ -eq [ItemType]::dn) -or 
+            ($_ -eq [ItemType]::s) -or 
+            ($_ -eq [ItemType]::i) -or 
+            ($_ -eq [ItemType]::p)
+        })] $ItemType = [ItemType]::p,
+
+        [Parameter(Mandatory = $true)] [Alias("k")] [string] $Key = $null,
+
+        [Parameter(Mandatory = $false)] [Alias("p")] [string] $Path,
+
+        [Parameter(Mandatory = $true)] 
+        [Alias("v")]
+        [ValidateScript({ 
+            ($_ -is [hashtable]) -or 
+            ($_ -is [string]) -or 
+            ($_ -is [int]) -or 
+            ($_ -is [double]) -or
+            ($_ -is [bool])
+        })] $Value,
+
+        [Parameter(Mandatory = $false)] [Alias("nocopy")] [switch] $NoCopyHashtable
+    )
+
+    Begin {
+        if ($ItemType -eq [ItemType]::p) {
+            if ($Value -is [hashtable]) { throw "Incorrect Value type." }
+            $val = $Value
+        }
+        elseif (($ItemType -eq [ItemType]::i) -or ($ItemType -eq [ItemType]::s) -or ($ItemType -eq [ItemType]::dn)) {
+            if (-not ($Value -is [hashtable])) { throw "Incorrect Value type." }
+            $val = ($NoCopyHashtable) ? $Value : (Copy-HashtableDeep -InputObject $Value )
+        }      
+        else { throw "Unhandled ItemType." }
+    }
+
+    Process {
+    }
+
+    End {
+        Write-Output [PSCustomObject] @{ 
+            [dni]::it = $ItemType; 
+            [dni]::k = $Key;
+            [dni]::p = $Path; 
+            [dni]::v = $val;
+            [dni]::vt = ($val.GetType().ToString() -split "\.") | Select-Object -Last 1
+        }
+    }
+}
+
+Set-Alias -Name ndni -Value New-DnItem
+
+#endregion
+
+#region l-1
 function Import-Dn {
     [CmdletBinding()]
     param (
@@ -72,8 +155,7 @@ function Import-Dn {
                 }
                 default { throw "File format '$ext' not supported." }
             }
-            $dnWrapper = [PSCustomObject] @{ "ItemType" = [ItemType]::DataNode ; "Path" = $FileInfo.FullName; "Value" = $dn; }
-            Write-Output $dnWrapper             
+            ndni -it [ItemType]::dn -k $null -p $FileInfo.FullName -v $dn -nocopy;
         }
         catch {
             Write-Error $_
@@ -86,66 +168,6 @@ function Import-Dn {
 
 Set-Alias -Name ipdn -Value Import-Dn
 
-function New-DnItem {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $false)] [string] 
-        [Alias("it")]
-        [ValidateScript({ 
-            ($_ -eq [ItemType]::Section) -or 
-            ($_ -eq [ItemType]::Item) -or 
-            ($_ -eq [ItemType]::Property)
-        })] $ItemType = [ItemType]::Property,
-
-        [Parameter(Mandatory = $true)] [Alias("k")] [string] $Key,
-
-        [Parameter(Mandatory = $false)] [Alias("p")] [string] $Path,
-
-        [Parameter(Mandatory = $true)] 
-        [Alias("v")]
-        [ValidateScript({ 
-            ($_ -is [hashtable]) -or 
-            ($_ -is [string]) -or 
-            ($_ -is [int]) -or 
-            ($_ -is [double]) -or
-            ($_ -is [bool])
-        })] $Value,
-
-        [Parameter(Mandatory = $false)] [Alias("nocopy")] [switch] $NoCopyHashtable
-    )
-
-    Begin {
-        if ($ItemType -eq [ItemType]::Section) {
-            if (-not ($Value -is [hashtable])) { throw "Incorrect Value type." }
-            $val = ($NoCopyHashtable) ? $Value : (Copy-HashtableDeep -InputObject $Value )
-        }
-        elseif ($ItemType -eq [ItemType]::Item) {
-            if (-not ($Value -is [hashtable])) { throw "Incorrect Value type." }
-            $val = ($NoCopyHashtable) ? $Value : (Copy-HashtableDeep -InputObject $Value )
-        }
-        elseif ($ItemType -eq [ItemType]::Property) {
-            if ($Value -is [hashtable]) { throw "Incorrect Value type." }
-            $val = $Value
-        }
-        else { throw "Unhandled ItemType." }
-    }
-
-    Process {
-    }
-
-    End {
-        Write-Output [PSCustomObject] @{ 
-            "ItemType" = $ItemType; 
-            "Key" = $Key;
-            "Path" = $Path; 
-            "Value" = $val;
-            "ValueType" = ($val.GetType().ToString() -split "\.") | Select-Object -Last 1
-        }
-    }
-}
-
-Set-Alias -Name ndni -Value New-DnItem
-
 function Set-DnItem {
     [CmdletBinding()]
     param (
@@ -156,36 +178,37 @@ function Set-DnItem {
     )
 
     Begin {
-        if (-not (($Item.ItemType -eq [ItemType]::Section) -or 
-            ($Item.ItemType -eq [ItemType]::Item) -or 
-            ($Item.ItemType -eq [ItemType]::Property))) { throw "Invalid Item Type." }
+        # validation of PSCustomObject not possible in param script
+        if ( -not @([ItemType]::s, [ItemType]::i, [ItemType]::p).Contains($Item.it))  { throw "Invalid Item Type." }
+        if (-not ($TargetDn.it -eq [ItemType]::dn)) { throw "Invalid Target Type." }
 
-        if (-not ($TargetDn.ItemType -eq [ItemType]::DataNode)) { throw "Invalid Target Type." }
+        # for items returned from gdni, take parent path
+        if ($null -eq $TargetPath) { $TargetPath = (Get-DnPath -Path $Item.p).ParentPath; }
+        $dnPath = Get-DnPath -Path $TargetPath;
+        $Root = $TargetDn.v;
 
-        $dnPath = $null -eq $TargetPath ? (Get-DnPath -Path $Item.Path).ParentPath : (Get-DnPath -Path $TargetPath);
-
-        if ($null -eq $dnPath -and $Item.ItemType -eq [ItemType]::Section) {
-            if ($NotOverride -and $TargetDn.Value.ContainsKey($Item.Key)) { throw "Overriding prohibited." }
+        if ($null -eq $dnPath.ParentPath -and $Item.it -eq [ItemType]::s) {
+            if ($NotOverride -and $Root.ContainsKey($Item.k)) { throw "Overriding prohibited." }
             else {
-                $TargetDn.Value[$Item.Key] = $Item.Value;
+                $Root[$Item.k] = $Item.v;
             }
         }
-        elseif ((-not ($null -eq $dnPath)) -and $dnPath.PathType -eq [ItemType]::Section -and $Item.ItemType -eq [ItemType]::Item) {
-            if (-not $TargetDn.Value.ContainsKey($dnPath.Section)) { $TargetDn.Value[$dnPath.Section] = @{} }
-            $targetSection = $TargetDn.Value[$dnPath.Section];
-            if ($NotOverride -and $targetSection.ContainsKey($Item.Key)) { throw "Overriding prohibited." }
+        elseif ((-not ($null -eq $dnPath.ParentPath)) -and $dnPath.PathType -eq [ItemType]::s -and $Item.it -eq [ItemType]::i) {
+            if (-not $Root.ContainsKey($dnPath.SectionPart)) { $Root[$dnPath.SectionPart] = @{} }
+            $targetSection = $Root[$dnPath.SectionPart];
+            if ($NotOverride -and $targetSection.ContainsKey($Item.k)) { throw "Overriding prohibited." }
             else {
-                $targetSection[$Item.Key] = $Item.Value;
+                $targetSection[$Item.k] = $Item.v;
             }
         }
-        elseif ((-not ($null -eq $dnPath)) -and $dnPath.PathType -eq [ItemType]::Item -and $Item.ItemType -eq [ItemType]::Property) {
-            if (-not $TargetDn.Value.ContainsKey($dnPath.Section)) { $TargetDn.Value[$dnPath.Section] = @{} }
-            $targetSection = $TargetDn.Value[$dnPath.Section];
-            if (-not $targetSection.ContainsKey($dnPath.Item)) { $targetSection[$dnPath.Item] = @{} }
-            $targetItem = $targetSection[$dnPath.Item];
-            if ($NotOverride -and $targetItem.ContainsKey($Item.Key)) { throw "Overriding prohibited." }
+        elseif ((-not ($null -eq $dnPath.ParentPath)) -and $dnPath.PathType -eq [ItemType]::i -and $Item.it -eq [ItemType]::p) {
+            if (-not $Root.ContainsKey($dnPath.SectionPart)) { $Root[$dnPath.SectionPart] = @{} }
+            $targetSection = $Root[$dnPath.SectionPart];
+            if (-not $targetSection.ContainsKey($dnPath.ItemPart)) { $targetSection[$dnPath.ItemPart] = @{} }
+            $targetItem = $targetSection[$dnPath.ItemPart];
+            if ($NotOverride -and $targetItem.ContainsKey($Item.k)) { throw "Overriding prohibited." }
             else {
-                $targetItem[$Item.Key] = $Item.Value;
+                $targetItem[$Item.k] = $Item.v;
             }
         }
         else {
@@ -215,28 +238,32 @@ function Remove-DnItem {
     }
 
     Process {
-        [hashtable] $Value = $dn.Value;
-        $MatchingSectionKeys = $Value.Keys | Where-Object { $_ -like $dnPath.Section }
+        [hashtable] $Root = $dn.v;
+        $MatchingSectionKeys = $Root.Keys | Where-Object { $_ -like $dnPath.SectionPart }
 
         foreach ($SectionKey in $MatchingSectionKeys) {
-            if ($dnPath.PathType -eq [ItemType]::Section) {
-                $Value.Remove($SectionKey);                   
+            if ($dnPath.PathType -eq [ItemType]::s) {
+                $Root.Remove($SectionKey);                   
             }
             else {
-                $Section = $Value[$SectionKey];                
-                $MatchingItemKeys = $Section.Keys | Where-Object { $_ -like $dnPath.Item }
+                $Section = $Root[$SectionKey];                
+                $MatchingItemKeys = $Section.Keys | Where-Object { $_ -like $dnPath.ItemPart }
                 foreach ($ItemKey in $MatchingItemKeys) {
-                    if ($dnPath.PathType -eq [ItemType]::Item) {
+                    if ($dnPath.PathType -eq [ItemType]::i) {
                         $Section.Remove($ItemKey);                            
                     }
                     else {
                         $Item = $Section[$ItemKey];                         
-                        $MatchingPropertyKeys = $Item.Keys | Where-Object { $_ -like $dnPath.Property }
+                        $MatchingPropertyKeys = $Item.Keys | Where-Object { $_ -like $dnPath.PropertyPart }
                         foreach ($PropertyKey in $MatchingPropertyKeys) {
                             $Item.Remove($PropertyKey);
                         }
+                        # if after romoving property, item remains empty, remove it
+                        if ($Item.Keys.Count -eq 0) { $Section.Remove($ItemKey); }
                     }
                 }
+                # if after romoving item, section remains empty, remove it
+                if ($Section.Keys.Count -eq 0) { $Value.Remove($SectionKey); }
             }
         }
     }
@@ -260,28 +287,28 @@ function Get-DnItem {
     }
 
     Process {
-        [hashtable] $Value = $dn.Value;
-        $MatchingSectionKeys = $Value.Keys | Where-Object { $_ -like $dnPath.Section }
+        [hashtable] $Root = $dn.v;
+        $MatchingSectionKeys = $Root.Keys | Where-Object { $_ -like $dnPath.SectionPart }
 
         foreach ($SectionKey in $MatchingSectionKeys) {
-            $Section = $Value[$SectionKey];
-            if ($dnPath.PathType -eq [ItemType]::Section) {
-                ndni -it ([ItemType]::Section) -k $SectionKey -p $SectionKey -v $Section                    
+            $Section = $Root[$SectionKey];
+            if ($dnPath.PathType -eq [ItemType]::s) {
+                ndni -it ([ItemType]::s) -k $SectionKey -p $SectionKey -v $Section                    
             }
             else {
-                $MatchingItemKeys = $Section.Keys | Where-Object { $_ -like $dnPath.Item }
+                $MatchingItemKeys = $Section.Keys | Where-Object { $_ -like $dnPath.ItemPart }
                 foreach ($ItemKey in $MatchingItemKeys) {
                     $Item = $Section[$ItemKey];              
-                    if ($dnPath.PathType -eq [ItemType]::Item) {
-                        $FullPath = "${SectionKey}/${ItemKey}";
-                        ndni -it ([ItemType]::Item) -k $ItemKey -p $FullPath -v $Item                            
+                    if ($dnPath.PathType -eq [ItemType]::i) {
+                        $FullPath = "${SectionKey}${PathDelimiter}${ItemKey}";
+                        ndni -it ([ItemType]::i) -k $ItemKey -p $FullPath -v $Item                            
                     }
                     else {
-                        $MatchingPropertyKeys = $Item.Keys | Where-Object { $_ -like $dnPath.Property }
+                        $MatchingPropertyKeys = $Item.Keys | Where-Object { $_ -like $dnPath.PropertyPart }
                         foreach ($PropertyKey in $MatchingPropertyKeys) {
                             $Property = $Item[$PropertyKey];
-                            $FullPath = "${SectionKey}/${ItemKey}/${PropertyKey}";
-                            ndni -it ([ItemType]::Property) -k $PropertyKey -p $FullPath -v $Property
+                            $FullPath = "${SectionKey}${PathDelimiter}${ItemKey}${PathDelimiter}${PropertyKey}";
+                            ndni -it ([ItemType]::p) -k $PropertyKey -p $FullPath -v $Property
                         }
                     }
                 }
@@ -294,6 +321,8 @@ function Get-DnItem {
 }
 
 Set-Alias -Name gdni -Value Get-DnItem
+#endregion
+
 
 Export-ModuleMember -Function *
 Export-ModuleMember -Alias *
